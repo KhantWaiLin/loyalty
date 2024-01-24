b279f6b8f0b46fd5e8acb2b31233cf3c153628f4
KhantWaiLin <khantwailin@bimgoc.com>
2024-01-24 16:50:53 +0630

    setting up routes for pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,29 @@
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import QR from "./pages/QR";
+import Reward from "./pages/Reward";
+import RewardDetail from "./pages/RewardDetail";
 
 const routes = [
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/qr",
+    element: <QR />,
+  },
+  {
+    path: "/reward",
+    element: <Reward />,
+  },
+  {
+    path: "/reward/:id",
+    element: <RewardDetail />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
   },
 ];
 
