70bf90bd250f9f1ebfd435c8bcdfe79c0313fa5a
KhantWaiLin <khantwailin@bimgoc.com>
2024-01-26 10:58:57 +0630

    adding footer

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -4,7 +4,10 @@ import Footer from "./Footer";
 const Layout = ({ children }) => {
   return (
     <div className="w-[100vw] flex  justify-center h-[100vh]">
-      <div className="w-[428px] h-full bg-slate-100">{children}</div>
+      <div className="w-[428px] justify-between relative h-full bg-slate-100">
+        {children}
+        <Footer />
+      </div>
     </div>
   );
 };
