55f9897ea72eed335386d98a88462e400004e157
Htoo Aung Lynn <htooaunglynn6@gmail.com>
2024-01-29 12:59:12 +0630

    use coupon ui layout

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,11 +4,13 @@ import Profile from "./pages/Profile";
 import QR from "./pages/QR";
 import Reward from "./pages/Reward";
 import RewardDetail from "./pages/RewardDetail";
+import UseCoupon from "./pages/use-coupon/UseCoupon";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/use-coupon" element={<UseCoupon />} />
     </Routes>
   );
 };
