de180ce77179324dd2d4af87ff322236c6110418
Htoo Aung Lynn <htooaunglynn6@gmail.com>
2024-01-30 17:22:49 +0630

    promotion detail, list ui fix

--- src/pages/promotion/list/PromotionList.jsx
+++ src/pages/promotion/list/PromotionList.jsx
@@ -3,48 +3,53 @@ import Footer from "../../../layouts/Footer";
 import PromotionItem from "./components/PromotionItem";
 import { useState, useEffect } from "react";
 
-function PromotionList({promotionList}) {
-  const baseURL = "https://loyaltybim.azurewebsites.net/api/Customer/GetPromotionListByBrandId";
+function PromotionList({ promotionList }) {
+  const baseURL =
+    "https://loyaltybim.azurewebsites.net/api/Customer/GetPromotionListByBrandId";
 
-  const authURL = "https://loyaltybim.azurewebsites.net/api/Authentication/Authenticate";
+  const authURL =
+    "https://loyaltybim.azurewebsites.net/api/Authentication/Authenticate";
 
   const [token, setToken] = useState("");
-  const [promotionData, setPromotionData] = useState('');
+  const [promotionData, setPromotionData] = useState("");
 
   useEffect(() => {
     fetch(authURL, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(auth)
-    }).then(response => response.json()).then(result => setToken(result.data.token));
-    
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(auth),
+    })
+      .then((response) => response.json())
+      .then((result) => setToken(result.data.token));
+
     if (token) {
       fetch(baseURL, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify(data)
-      }).then(response => response.json()).then(result => setPromotionData(result.value));
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((result) => setPromotionData(result.value));
     }
-
   }, [token]);
 
   const auth = {
-    "userName": "09422924858",
-    "password": "jujuJu1",
-    "userType": 2
+    userName: "09422924858",
+    password: "jujuJu1",
+    userType: 2,
   };
 
   const data = {
-    "keyword": "",
-    "rowLimit": 10,
-    "currentPage": 1,
-    "sortBy": "",
-    "isDesc": true,
-    "brandId": "265f0dff-a30a-11ed-b26e-6045bdd63acb",
-    "filterDate": null
+    keyword: "",
+    rowLimit: 10,
+    currentPage: 1,
+    sortBy: "",
+    isDesc: true,
+    brandId: "265f0dff-a30a-11ed-b26e-6045bdd63acb",
+    filterDate: null,
   };
 
   let promotionListData = "";
@@ -54,29 +59,50 @@ function PromotionList({promotionList}) {
   } else {
     return null;
   }
-  
+
   return (
-    <main className="flex flex-col h-full border-2">
-      <section className="flex items-center justify-center basis-1/12">
-        <h1>Promotion List</h1>
-      </section>
-      <section className="px-2 basis-1/12">
-        <ul className="flex items-center justify-between w-1/2 h-full">
-          <li className="cursor-pointer">Available</li>
-          <li className="cursor-pointer">Used</li>
-          <li className="cursor-pointer">Expired</li>
+    <main className="relative flex flex-col h-full">
+      {/* header */}
+      <header className="flex bg-blue-800 basis-2/12">
+        <article className="mt-7">
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5 8.25 12l7.5-7.5"
+              />
+            </svg>
+          </button>
+        </article>
+        <h1 className="text-center text-white mt-7 ms-36">Promotion</h1>
+        {/* <article className="absolute z-20 w-32 h-32 overflow-hidden bg-blue-200 rounded-full -right-4 -top-4"></article> */}
+      </header>
+      {/* header */}
+
+      {/* body */}
+      <section className="bg-white z-30 rounded-t-2xl absolute top-[90px] left-0 w-full h-[520px] basis-10/12 pt-[24px] px-[20px]">
+        <ul className="max-h-[445px] overflow-auto">
+          {promotionListData &&
+            promotionListData.map((promotion, index) => (
+              <PromotionItem promotion={promotion} key={index} />
+            ))}
         </ul>
       </section>
-      <section className="px-2 basis-9/12">
-        <ul className="mt-1">
-          {
-            promotionListData && promotionListData.map((promotion, index) => <PromotionItem promotion={promotion} key={index}/>)
-          }
-        </ul>
-      </section>
-      <section className="basis-1/12">
+      {/* body */}
+
+      {/* footer */}
+      <footer className="absolute bottom-0 left-0 z-50 w-full basis-1/12">
         <Footer />
-      </section>
+      </footer>
+      {/* footer */}
     </main>
   );
 }
--- src/pages/promotion/list/components/PromotionItem.jsx
+++ src/pages/promotion/list/components/PromotionItem.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function PromotionItem({ promotion }) {
+  let navigate = useNavigate();
+
+  const handleDetail = (id) => {
+    navigate(`/promotion-detail/${id}`);
+  };
+
   return (
-    <li className="flex items-center justify-between px-3 py-2 mb-3 border-2 border-black rounded-md">
-      <span>{promotion.name}</span>
-      <span>
-        <Link to={`/promotion-detail/${promotion.id}`} className="px-2 border-2 rounded-full">Detail</Link>
-      </span>
+    <li
+      onClick={() => handleDetail(promotion.id)}
+      className="cursor-pointer h-[260px] mb-1 p-[10px] w-full bg-white"
+    >
+      <img
+        src={promotion.image}
+        alt={promotion.name}
+        className="h-[180px] w-full rounded-md mb-[8px]"
+      />
+      <p lassName="text-base font-normal not-italic text-[#48505E]">
+        {promotion.name}
+      </p>
+      <p className="text-sm font-normal mt-[8px] not-italic text-[#989FAD]">
+        {promotion.startDate}
+      </p>
     </li>
   );
 }
