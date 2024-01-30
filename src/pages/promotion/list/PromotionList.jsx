173a7e0502255be49f424a5e99e5e18ac5adbe84
Htoo Aung Lynn <htooaunglynn6@gmail.com>
2024-01-30 12:38:50 +0630

    promotion api name list

--- src/pages/promotion/list/PromotionList.jsx
+++ src/pages/promotion/list/PromotionList.jsx
@@ -1,8 +1,60 @@
 import React from "react";
 import Footer from "../../../layouts/Footer";
 import PromotionItem from "./components/PromotionItem";
+import { useState, useEffect } from "react";
 
 function PromotionList({promotionList}) {
+  const baseURL = "https://loyaltybim.azurewebsites.net/api/Customer/GetPromotionListByBrandId";
+
+  const authURL = "https://loyaltybim.azurewebsites.net/api/Authentication/Authenticate";
+
+  const [token, setToken] = useState("");
+  const [promotionData, setPromotionData] = useState('');
+
+  useEffect(() => {
+    fetch(authURL, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(auth)
+    }).then(response => response.json()).then(result => setToken(result.data.token));
+    
+    if (token) {
+      fetch(baseURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(data)
+      }).then(response => response.json()).then(result => setPromotionData(result.value.data));
+    }
+
+  }, [token]);
+
+  const auth = {
+    "userName": "09422924858",
+    "password": "jujuJu1",
+    "userType": 2
+  };
+
+  const data = {
+    "keyword": "",
+    "rowLimit": 10,
+    "currentPage": 1,
+    "sortBy": "",
+    "isDesc": true,
+    "brandId": "265f0dff-a30a-11ed-b26e-6045bdd63acb",
+    "filterDate": null
+  };
+
+  let promotionListData = "";
+
+  if (promotionData) {
+    promotionListData = promotionData.data;
+  } else {
+    return null;
+  }
+  
   return (
     <main className="flex flex-col h-full border-2">
       <section className="flex items-center justify-center basis-1/12">
@@ -17,11 +69,9 @@ function PromotionList({promotionList}) {
       </section>
       <section className="px-2 basis-9/12">
         <ul className="mt-1">
-          <PromotionItem name="Redeem Product" />
-          <PromotionItem name="Coupon Code" />
-          <PromotionItem name="Redeem Product" />
-          <PromotionItem name="Coupon Code" />
-          <PromotionItem name="Coupon Code" />
+          {
+            promotionListData && promotionListData.map((promotion, index) => <PromotionItem promotion={promotion} key={index}/>)
+          }
         </ul>
       </section>
       <section className="basis-1/12">
--- src/pages/promotion/list/components/PromotionItem.jsx
+++ src/pages/promotion/list/components/PromotionItem.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function PromotionItem({ name }) {
+function PromotionItem({ promotion }) {
   return (
     <li className="flex items-center justify-between px-3 py-2 mb-3 border-2 border-black rounded-md">
-      <span>{name}</span>
+      <span>{promotion.name}</span>
       <span>
         <button className="px-2 border-2 rounded-full">Use Code</button>
       </span>
