0340c83ae5e5ec2ddbcd191efccf42067250b75f
KhantWaiLin <khantwailin@bimgoc.com>
2024-01-31 10:33:53 +0630

    remove footer from login page

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Footer from "./footer/Footer";
+import { useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
+  const location = useLocation();
+
   return (
     <div className="w-[100vw] flex  justify-center h-[100vh]">
       <div className="w-[428px] justify-between relative h-full bg-gray-50">
         {children}
-        <Footer />
+
+        {location?.pathname !== "/" && <Footer />}
       </div>
     </div>
   );
