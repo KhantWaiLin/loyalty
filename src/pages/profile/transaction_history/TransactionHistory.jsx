4e626ee9d59918a13b1a911a1e9925b1988f3fd3
Htoo Aung Lynn <htooaunglynn6@gmail.com>
2024-02-01 15:15:11 +0630

    point api data testing finish

--- src/pages/profile/transaction_history/TransactionHistory.jsx
+++ src/pages/profile/transaction_history/TransactionHistory.jsx
@@ -4,6 +4,7 @@ import { getUserBrandMemberId } from "../../../utils/getBrandUserId";
 import api from "../../../api/api";
 import { format } from "date-fns";
 import PointList from "./components/PointList";
+import { Link } from "react-router-dom";
 
 import Loader from "../../../components/loader/Loader";
 
@@ -14,7 +15,20 @@ const TransactionHistory = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const { transaction_history } = api_routes;
-  const { brand_id, user_id } = getUserBrandMemberId();
+  const { brand_id, user_id, member_id } = getUserBrandMemberId();
+
+  useEffect(() => {
+    get_transaction();
+    // eslint-disable-next-line
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col items-center justify-center transaction-history-wrapper">
+        <Loader />
+      </div>
+    );
+  }
 
   const get_transaction = async () => {
     setIsLoading(true);
@@ -29,29 +43,15 @@ const TransactionHistory = () => {
     setIsLoading(false);
   };
 
-  useEffect(() => {
-    get_transaction();
-    // eslint-disable-next-line
-  }, []);
-  if (isLoading) {
-    return (
-      <div className="flex flex-col items-center justify-center transaction-history-wrapper">
-        <Loader />
-      </div>
-    );
-  }
-
-  const handleBack = () => {
-    console.log("back");
-  };
-
   return (
     <div className="relative flex flex-col w-full overflow-scroll transaction-history-wrapper no-scrollbar">
       <header className="flex flex-col z-30 bg-indigo-700 basis-2/12 ps-[20px] pr-[42%]">
         <section className="flex justify-between mt-[30px]">
-          <div className="flex flex-col items-start justify-start w-6 h-6">
+          <Link
+            to="/profile"
+            className="flex flex-col items-start justify-start w-6 h-6"
+          >
             <svg
-              onClick={handleBack}
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -65,25 +65,29 @@ const TransactionHistory = () => {
                 d="M15.75 19.5 8.25 12l7.5-7.5"
               />
             </svg>
-          </div>
+          </Link>
           <h1 className="text-white">Point History</h1>
         </section>
       </header>
 
       {/* point history list */}
       <main className=" z-50 bg-white absolute left-0 rounded-t-2xl top-20 w-full h-[485px] basis-10/12 overflow-auto no-scrollbar pt-3">
-          <PointList/>
-          <PointList/>
-          <PointList/>
-          <PointList/>
-          <PointList/>
-          <PointList/>
-          <PointList/>
-          <PointList/>
-          <PointList/>
-          <PointList/>
-          <PointList/>
-          <div className="text-gray-400 text-[13px] font-normal  leading-tight mb-1 text-center">No More Result!</div>
+        {data &&
+          data.map((point, index) => {
+            return (
+              <PointList
+                key={index}
+                point={point.point}
+                collectedType={point.collectedType}
+                isIn={point.isIn}
+                date={format(point.date, "MMMM dd, yyyy")}
+              />
+            );
+          })}
+
+        <div className="text-gray-400 text-[13px] font-normal  leading-tight mb-1 text-center">
+          No More Result!
+        </div>
       </main>
       {/* point history list */}
     </div>
@@ -91,40 +95,3 @@ const TransactionHistory = () => {
 };
 
 export default TransactionHistory;
-
-{
-  /* <div className="flex flex-col w-full px-6 py-4 overflow-scroll transaction-history-wrapper">
-  <h1 className="text-[24px] w-full flex justify-center font-medium mb-10">
-    Transaction History
-  </h1>
-  <table className="w-full border border-collapse border-gray-500">
-    <thead>
-      <tr>
-        <th className="p-2 border border-gray-500">Point</th>
-        <th className="p-2 border border-gray-500">Collected Type</th>
-        <th className="p-2 border border-gray-500">Is In</th>
-        <th className="p-2 border border-gray-500">Date</th>
-      </tr>
-    </thead>
-    <tbody>
-      {data?.map((item) => {
-        const date = new Date(item?.date);
-        const formattedDate = format(date, "yyyy-MM-dd HH:mm");
-
-        return (
-          <tr key={item?.date}>
-            <td className="p-2 border border-gray-500">{item?.point}</td>
-            <td className="p-2 border border-gray-500">
-              {item?.collectedType}
-            </td>
-            <td className="p-2 border border-gray-500">
-              {item?.isIn?.toString()}
-            </td>
-            <td className="p-2 border border-gray-500">{formattedDate}</td>
-          </tr>
-        );
-      })}
-    </tbody>
-  </table>
-</div> */
-}
--- src/pages/profile/transaction_history/components/PointList.jsx
+++ src/pages/profile/transaction_history/components/PointList.jsx
@@ -1,50 +1,28 @@
-function PointList() {
-    return (
-        <div className="h-[65px] border-b-2 px-4 mb-1 z-10">
-            <section className="flex h-full py-1 gap-x-1">
-                <article className=" basis-1/4">
-                    <div className="bg-yellow-400 rounded-full w-14 h-14">
-
-                    </div>
-                </article>
-                <article className="flex flex-col justify-between pb-1 basis-2/4">
-                    <div className="text-base font-medium leading-normal text-gray-600">
-                        Point Added
-                    </div>
-                    <div className="text-gray-400 text-[13px] font-normal  leading-tight">
-                        September 7, 2023
-                    </div>
-                </article>
-                <article className="text-base font-medium leading-normal text-green-500 basis-1/4">
-                    + 200 Points
-                </article>
-            </section>
-        </div>
-    );
+function PointList({ index, point, collectedType, isIn, date }) {
+  return (
+    <div key={index} className="h-[65px] border-b-2 px-4 mb-1 z-10">
+      <section className="flex h-full py-1 gap-x-1">
+        <article className=" basis-1/4">
+          <div className="bg-yellow-400 rounded-full w-14 h-14"></div>
+        </article>
+        <article className="flex flex-col justify-between pb-1 basis-2/4">
+          <div className="text-base font-medium leading-normal text-gray-600">
+            {collectedType}
+          </div>
+          <div className="text-gray-400 text-[13px] font-normal  leading-tight">
+            {date}
+          </div>
+        </article>
+        <article
+          className={`text-sm font-medium leading-normal  basis-1/4 ${
+            isIn === true ? "text-green-500" : "text-red-500"
+          }`}
+        >
+          + {point} Points
+        </article>
+      </section>
+    </div>
+  );
 }
 
 export default PointList;
-
-
-{/* <div
-    className="w-full h-[80px] flex gap-[8px] bg-green-400 border-b-2"
-  >
-    <section className="flex justify-center basis-1/4 bg-yello">
-      <div className="flex items-center justify-center w-16 h-16 rounded-lg bg-indigo-50">
-        <div className="h-10 w-18 mix-blend-darken">
-          1
-        </div>
-      </div>
-    </section>
-    <section className="flex flex-col basis-2/4 justify-evenly">
-      <p className="text-sm font-medium leading-normal text-gray-600">
-        name
-      </p>
-      <div className=" text-gray-400 text-[10px] font-normal  leading-tight">
-      date
-    </div>
-    </section>
-    <section className="flex items-center h-full basis-1/4">
-      point
-    </section>
-  </div> */}
