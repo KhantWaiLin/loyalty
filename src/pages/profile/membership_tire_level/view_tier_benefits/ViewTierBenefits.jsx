a3fe5f46134df3a03d26fa2d31ff74ddb84d289c
Htoo Aung Lynn <htooaunglynn6@gmail.com>
2024-02-02 15:34:17 +0630

    member Tier Benefits ui and api finish

--- src/pages/profile/membership_tire_level/view_tier_benefits/ViewTierBenefits.jsx
+++ src/pages/profile/membership_tire_level/view_tier_benefits/ViewTierBenefits.jsx
@@ -7,6 +7,7 @@ import { api_routes } from "../../../../utils/apiRoute";
 import { getUserBrandMemberId } from "../../../../utils/getBrandUserId";
 
 import "./ViewTierBenefits.scss";
+import { Link } from "react-router-dom";
 
 const ViewTierBenefits = () => {
   const [data, setData] = useState(null);
@@ -37,34 +38,76 @@ const ViewTierBenefits = () => {
   }
 
   return (
-    <div className="member-tire-wrapper flex flex-col py-4 px-6 w-full overflow-scroll">
-      <h1 className="text-[24px] w-full flex justify-center font-medium mb-10">
-        Member Tire Level
-      </h1>
-      <table className="w-full border-collapse border border-gray-500">
-        <thead>
-          <tr>
-            <th className="border border-gray-500 p-2">Name</th>
-            <th className="border border-gray-500 p-2">Image</th>
-            <th className="border border-gray-500 p-2">Usage</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.map((item) => (
-            <tr key={item?.name}>
-              <td className="border border-gray-500 p-2">{item?.name}</td>
-              <td className="border border-gray-500 p-2">
-                <img
-                  src={item?.image}
-                  alt="level-img"
-                  className="w-[50px] h-[50px]"
-                />
-              </td>
-              <td className="border border-gray-500 p-2">{item?.usage}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="member-tire-wrapper relative flex flex-col w-full overflow-scrol no-scrollbar">
+      <header className="flex flex-col z-30 bg-indigo-700 basis-2/12 ps-[20px] pr-[40%]">
+        <section className="flex justify-between mt-[20px]">
+          <Link
+            to="/profile/membership-tire-level"
+            className="flex flex-col items-start justify-start w-6 h-6"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-white cursor-pointer"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5 8.25 12l7.5-7.5"
+              />
+            </svg>
+          </Link>
+          <h1 className="text-white">Tier Benefits</h1>
+        </section>
+      </header>
+
+      {/* Member Tire Level */}
+      <main className="z-50 bg-white absolute left-0 rounded-t-2xl top-16 w-full h-[470px] basis-10/12 overflow-auto no-scrollbar flex flex-col">
+        <h1 className="font-semibold text-base text-center my-[20px]">
+          SateKyite Point Membership Level
+        </h1>
+
+        <div class="relative overflow-x-auto mx-[12px] rounded-md">
+          <table class="w-full text-sm text-center">
+            <thead class="text-base bg-indigo-700 text-white ">
+              <tr>
+                <th
+                  scope="col"
+                  class="px-6 py-2 border-r border-r-slate-100 font-normal text-base"
+                >
+                  Usage
+                </th>
+                <th scope="col" class="px-6 py-2 font-normal text-base">
+                  Tire Level
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((item, index) => {
+                return (
+                  <tr key={index} class="odd:bg-gray-100">
+                    <td class="px-6 py-2 text-sm font-medium border-r border-r-slate-100">
+                      {item?.usage}
+                    </td>
+                    <td class="px-6 py-2 font-medium text-sm flex justify-start items-center">
+                      <img
+                        src={item?.image}
+                        alt="level-img"
+                        className="w-[23px] h-[23px]"
+                      />
+                      <p className="text-sm ps-2">{item?.name}</p>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </main>
+      {/* Member Tire Level */}
     </div>
   );
 };
