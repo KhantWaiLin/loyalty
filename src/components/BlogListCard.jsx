92b48474327c6e5d4b8fdbab63d9ac8fb82a694c
pyaephyoaung <pyaephyoaung@bimgoc.com>
2024-02-04 21:07:02 +0630

    done

--- src/components/BlogListCard.jsx
+++ src/components/BlogListCard.jsx
@@ -1,57 +1,47 @@
-// BlogListCard.js
-
 import React from "react";
-import Like from "../assets/icons/like-icon.svg";
-import Comment from "../assets/icons/comment-icon.svg";
 
 const cardStyle = {
-  width: '420px',
+  width: '388px',
   height: '220px',
   border: '1px solid #F0F1F3',
   borderRadius: '20px',
   padding: '10px',
 };
 
-const BlogListCard = ({ blog, onClick }) => {
+const imageSectionStyle = {
+  width: '368px',
+  height: '140px',
+  borderRadius: '8px',
+  overflow: 'hidden',
+  objectFit: 'cover',
+};
+
+function BlogListCard({ blog, link }) {
+  const url = '/blog/';
+
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = new Date(dateString).toLocaleDateString(undefined, options);
+    return formattedDate;
+  };
+
   return (
-    <div
-      className="w-full h-full flex flex-col justify-between items-center relative bg-[#FFF] rounded-[20px] border-[1px] border-[#F0F1F3] p-[10px] cursor-pointer"
-      onClick={onClick}
-      style={cardStyle}
-    >
-      <div className="h-[180px] w-full">
+    <a href={url+link} style={cardStyle}>
+      <div style={imageSectionStyle}>
         <img
-          src={blog.image}
-          alt="blog image"
-          className="w-full h-full object-cover rounded-lg"
+          className="w-full h-full object-contain"
+          src={blog?.image}
+          alt="Card Image"
         />
       </div>
-      <div className="w-full flex flex-col gap-1 p-4">
-        <div className="w-full flex flex-col gap-1">
-          <h4 className="w-full text-lg md:text-xl lg:text-xl font-medium leading-6 justify-start">
-            {blog.title}
-          </h4>
-          <p className="text-[#858D9D] leading-4 text-sm md:text-base lg:text-sm font-normal">
-            {blog?.description}
-          </p>
-        </div>
-        <div className="w-full flex justify-start items-center gap-5">
-          <div className="flex items-center gap-2">
-            <button className="w-5 h-5">
-              <img src={Like} alt="Like Icon" className="w-full h-full" />
-            </button>
-            <p className="text-[#667085] text-sm md:text-base lg:text-lg">{blog?.likes}</p>
-          </div>
-          <div className="flex items-center gap-2">
-            <button className="w-5 h-5">
-              <img src={Comment} alt="Comment Icon" className="w-full h-full" />
-            </button>
-            <p className="text-[#667085] text-sm md:text-base lg:text-lg">{blog?.comments}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+      <p className="text-base font-normal not-italic text-[#48505E]">
+        {blog?.title}
+      </p>
+      <p className="text-sm font-normal mt-[8px] not-italic text-[#989FAD]">
+        {formatDate(blog?.startDate)}
+      </p>
+    </a>
   );
-};
+}
 
 export default BlogListCard;
--- src/pages/blog/SavedBlogs.jsx
+++ src/pages/blog/SavedBlogs.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import BlogListCard from "../../components/BlogListCard";
-import BlogNav from "./BlogNav";
+import TabNavigation from "./TabNavigation";
+import SavedBlogCard from "./SavedBlogCard";
 
 import Loader from "../../components/loader/Loader";
 import api from "../../api/api";
@@ -44,7 +45,7 @@ const saveStyle = {
   borderRadius: '5px',
 };
 
-const BlogList = () => {
+const SavedBlogs = () => {
   const [blogList, setBlogList] = useState(null);
   const { blog_list } = api_routes;
   const [isLoading, setIsLoading] = useState(false);
@@ -78,25 +79,20 @@ const BlogList = () => {
 
   return (
     <div className="text-black-500 text-lg">
-      <a style={iconStyle} href="/home">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-        </svg>
-      </a>
-      <h1 style={headingStyle}>Blogs</h1>
-      <a style={saveStyle} href="/blogsaved">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
-        </svg>
-      </a>
-      {/* <BlogNav /> */}
-      <div style={cardListStyle} className="no-scrollbar">
-        {blogList?.map((blog) => (
-          <BlogListCard key={blog.id} blog={blog} link={blog.id} />
-        ))}
-      </div>
+    <a style={iconStyle} href="/bloglist">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+      </svg>
+    </a>
+    <h1 style={headingStyle}>Blogs</h1>
+    <TabNavigation/>
+    <div style={cardListStyle} className="no-scrollbar">
+      {blogList?.map((blog) => (
+        <SavedBlogCard key={blog.id} blog={blog} link={blog.id} />
+      ))}
     </div>
+  </div>
   );
 };
 
-export default BlogList;
+export default SavedBlogs;
