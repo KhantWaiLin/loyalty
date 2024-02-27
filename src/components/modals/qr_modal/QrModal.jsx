2515d447807cd38f4510fe29ec7934049dae211a
pyaephyoaung <pyaephyoaung@bimgoc.com>
2024-02-27 14:32:41 +0630

    coupon_qr_modal

--- src/components/modals/qr_modal/QrModal.jsx
+++ src/components/modals/qr_modal/QrModal.jsx
@@ -12,12 +12,12 @@ const QrModal = ({ setIsClick, image }) => {
       <div className="qr-container w-full flex flex-col justify-center items-center p-4 bg-white z-30 shadow-lg rounded-lg">
         <div className="w-[250px] h-[250px]">
           {image ? (
-            <img src={image} />
+            image 
           ) : (
             <img src={DefaultImage} alt="qr" className="w-full h-full" />
           )}
         </div>
-        <p className="mb-5 text-[#667085] text-[14px] leading-5">
+        <p className="mb-5 mt-5 text-[#667085] text-[14px] leading-5">
           Show QR Code to Collect Reward
         </p>
         <div className="flex w-full gap-5 justify-between items-center">
